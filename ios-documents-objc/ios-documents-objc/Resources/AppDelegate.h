e071f291180e7c554739a5bd88028381ecd5aa9e
stdemicheli <stdemicheli@gmail.com>
2018-10-03 21:29:30 +0200

    Initial commit.

---
+++ ios-documents-objc/ios-documents-objc/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-documents-objc
+//
+//  Created by De MicheliStefano on 03.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-documents-objc/ios-documents-objc/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-documents-objc
+//
+//  Created by De MicheliStefano on 03.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
